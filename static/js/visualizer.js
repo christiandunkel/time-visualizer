318860176f140d04b50aca824043c81efe0f1cc1
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-08-12 16:00:52 +0200

    added basic navigation layout and dark mode

---
+++ static/js/visualizer.js
@@ -0,0 +1,11 @@
+let html = document.getElementsByTagName("html")[0];
+
+// dark mode
+
+let darkmode = false;
+document.getElementById("darkmode-toggle").addEventListener("click", function () {
+    // toggle darkmode value
+    darkmode = !darkmode;
+    // toggle darkmode class according to value
+    html.classList[darkmode ? "add" : "remove"]("darkMode");
+});
