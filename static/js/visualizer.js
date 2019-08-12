efd110b79c32645a71ca616c9b185165b91b1ade
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-08-12 18:07:49 +0200

    simplified navigation design; added button to change chart type

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -1,11 +1,11 @@
-let html = document.getElementsByTagName("html")[0];
+let html = document.getElementsByTagName('html')[0];
 
 // dark mode
 
 let darkmode = false;
-document.getElementById("darkmode-toggle").addEventListener("click", function () {
+document.getElementById('toggle-darkmode').addEventListener('click', function () {
     // toggle darkmode value
     darkmode = !darkmode;
     // toggle darkmode class according to value
-    html.classList[darkmode ? "add" : "remove"]("darkMode");
+    html.classList[darkmode ? 'add' : 'remove']('darkMode');
 });
