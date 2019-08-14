b10022759b7eefc51febc3e7af9a28ab8bfa0719
Christian Dunkel <christiandunkel.contact@gmail.com>
2019-08-14 23:45:40 +0200

    added animated window 'open and close' mechanic

--- static/js/visualizer.js
+++ static/js/visualizer.js
@@ -17,6 +17,26 @@ let body = _.tag('body')[0];
  * ==========
  */
 
+// load data button
+let data_load_btn = _.id('load-data');
+let data_load_window = _.id('data-load-window');
+_.addClick(data_load_btn, function () {
+    _.addClass(data_load_window, 'visible');
+});
+
+// close 'data load window' button
+let data_load_window_close_btn = _.class('close', data_load_window)[0];
+_.addClick(data_load_window_close_btn, function () {
+    _.removeClass(data_load_window, 'visible');
+});
+
+// close 'data load window' on click on 'blured' background
+let data_load_window_blur = _.class('blur', data_load_window)[0];
+_.addClick(data_load_window_blur, function () {
+    _.removeClass(data_load_window, 'visible');
+});
+
+// dark mode button
 let darkmode = false;
 let darkmode_toggle = _.id('toggle-darkmode');
 _.addClick(darkmode_toggle, function () {
